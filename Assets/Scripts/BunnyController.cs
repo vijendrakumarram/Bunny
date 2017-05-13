6dfbc261ff565c80795cc27a7636dd18a9a3527a
vijendrakumarram <vijendrakumarram@gmail.com>
2017-05-13 10:09:22 +0530

    Added

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour {
+
+	private Rigidbody2D myRigidBody;
+	private Animator myAnim;
+
+	private Transform myTransform;
+	public float bunnyJumpForce=500f;
+	// Use this for initialization
+	void Start () 
+	{
+		myRigidBody = GetComponent<Rigidbody2D> ();	
+		myTransform = transform;
+		myAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonUp ("Jump")) 
+		{
+			myRigidBody.AddForce (myTransform.up*bunnyJumpForce);
+		}	
+
+		myAnim.SetFloat ("vVelocity", myRigidBody.velocity.y);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Enemy")) 
+		{
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour {
+
+	public float speed=5f;
+
+	private Transform cactusTransform;
+	// Use this for initialization
+	void Start ()
+	{
+		cactusTransform = transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		cactusTransform.position+= Vector3.left * speed * Time.deltaTime;
+	}
+
+
+
+
+}
